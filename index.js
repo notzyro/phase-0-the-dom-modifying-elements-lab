05bb330dd4d4808436032c2706b560dc86d9d4f8
Edward Angelo Salas <zyro670@gmail.com>
2022-04-03 00:28:25 -0400

    commit tree dc73b91d1e5d18a6ba1f10beb1bdb6af38606070

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.getElementById("main").remove(); // Passes test case 1
+
+const newHeader = document.createElement("h1"); // Should pass case 2
+newHeader.id = "victory"; // Should pass case 3
+newHeader.innerHTML = "YOUR-NAME is the champion"; // Should pass case 4
+
+document.body.appendChild(newHeader); // Add it to the DOM
